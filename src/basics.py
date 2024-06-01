5d34d3c0d08964b9e36782b8506063be80864798
tvsiddhu <tv.siddhu@gmail.com>
2024-06-01 23:39:02 +0800

    basic code from AI4I-python intro

---
+++ src/basics.py
@@ -0,0 +1,46 @@
+monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
+
+# Calculate year_savings using monthly_savings and num_months
+year_savings = monthly_savings * num_months
+print(year_savings)
+
+# Print the type of year_savings
+print(type(year_savings))
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
+
+
+a_list = [1, 3, 4, 2]
+b_list = [[1, 2, 3], [4, 5, 7]]
+c_list = [1 + 2, "a" * 5, 3]
+
+print(a_list)
+print(b_list)
+print(c_list)
+
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom",bath]]
+
+# Print out house
+print(house)
+
+# Print out the type of house
+print(type(house))
