1a95f3b08bd45e2474374c9ab837b01a05ffb214
tvsiddhu <tv.siddhu@gmail.com>
2024-06-12 20:05:29 +0530

    Completed Dictionaries and Pandas chapter.

--- src/intermediate.py
+++ src/intermediate.py
@@ -134,7 +134,7 @@ plt.title(title)
 # After customizing, display the plot
 plt.show()
 
-# Use tick_val and tick_lab as inputs to the xticks() function to make the the plot more readable.
+# Use tick_val and tick_lab as inputs to the xticks() function to make the plot more readable.
 # As usual, display the plot with plt.show() after you've added the customizations.
 
 # Scatter plot
@@ -184,16 +184,16 @@ plt.xticks([1000, 10000, 100000], ['1k', '10k', '100k'])
 plt.show()
 
 # map continents onto colors
-dict = {
-    'Asia':'red',
-    'Europe':'green',
-    'Africa':'blue',
-    'Americas':'yellow',
-    'Oceania':'black'
+my_dict = {
+    'Asia': 'red',
+    'Europe': 'green',
+    'Africa': 'blue',
+    'Americas': 'yellow',
+    'Oceania': 'black'
 }
 
-wbank['color'] = wbank['cont'].map(dict)
-cont = wbank['color'].tolist()
+wbank['color'] = wbank['cont'].map(my_dict)
+my_color = wbank['color'].tolist()
 
 # Specify c and alpha inside plt.scatter() to color each bubble by continent and to
 # make the bubbles 20% transparent. To see the different continents, also use the alpha
@@ -201,7 +201,7 @@ cont = wbank['color'].tolist()
 # more readable.
 
 # Specify c and alpha inside plt.scatter()
-plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) / 570000, c = cont, alpha = 0.8)
+plt.scatter(x=gdp_cap, y=life_exp, s=np.array(pop) / 570000, c=my_color, alpha=0.8)
 
 # Previous customizations
 plt.xscale('log')
@@ -215,4 +215,205 @@ plt.grid(True)
 
 # Display the plot
 plt.show()
+
+# using dictionaries
+
+# Use the index() method on countries to find the index of 'germany'. Store this index as ind_ger.
+# Use ind_ger to access the capital of Germany from the capitals list. Print it out.
+
+# Definition of countries and capital
+
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger = countries.index('germany')
+
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
+
+# With the strings in countries and capitals, create a dictionary called europe with 4 key:value pairs. Beware of
+# capitalization! Make sure you use lowercase characters everywhere.
+
+# From string in countries and capitals, create dictionary europe
+europe = {
+    'spain': 'madrid',
+    'france': 'paris',
+    'germany': 'bonn',
+    'norway': 'oslo'
+}
+
+# Print europe
+print(europe)
+
+# Check out which keys are in europe by calling the keys() method on europe. Print out the result.
+# Print out the value that belongs to the key 'norway'.
+
+# Print out the keys in europe
+print(europe.keys())
+print(europe.values())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+
+# Add the key 'italy' with the value 'rome' to europe.
+# To assert that 'italy' is now a key in europe, print out 'italy' in europe.
+# Add another key:value pair to europe: 'poland' is the key, 'warsaw' is the corresponding value.
+# Print out europe.
+
+# Add italy to europe
+europe['italy'] = 'rome'
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
+
+# Update the capital of germany to 'berlin'.
+# Remove australia from europe.
+# Print out europe to see if your update worked.
+europe = {'spain': 'madrid', 'france': 'paris', 'germany': 'bonn',
+          'norway': 'oslo', 'italy': 'rome', 'poland': 'warsaw',
+          'australia': 'vienna'}
+
+print("------------------")
+print(europe)
+print("------------------")
+
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+del (europe['australia'])
+
+# Print europe
+print(europe)
+
+# Dictionary of dictionaries
+europe = {'spain': {'capital': 'madrid', 'population': 46.77},
+          'france': {'capital': 'paris', 'population': 66.03},
+          'germany': {'capital': 'berlin', 'population': 80.62},
+          'norway': {'capital': 'oslo', 'population': 5.084}}
+
+# Use chained square brackets to select and print out the capital of France.
+# Create a dictionary, named data, with the keys 'capital' and 'population'. Set them to 'rome' and 59.83, respectively.
+# Add a new key-value pair to europe; the key is 'italy' and the value is data, the dictionary you just built.
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'capital': 'rome', 'population': 59.83}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
+
+# Pandas
+
+# Use the pre-defined lists to create a dictionary called my_dict. There should be three key value pairs:
+# key 'country' and value names.
+# key 'drives_right' and value dr.
+# key 'cars_per_cap' and value cpc.
+# Use pd.DataFrame() to turn your dict into a DataFrame called cars.
+# Print out cars and see how beautiful it is.
+
+# Pre-defined lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr = [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+# Create dictionary my_dict with three key:value pairs: my_dict
+my_dict = {
+    'country': names,
+    'drives_right': dr,
+    'cars_per_cap': cpc
+}
+
+cars = pd.DataFrame(my_dict)
+
+# Print cars
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = row_labels
+
+print(cars)
+
+# Run the code with Run Code and assert that the first column should actually be used as row labels.
+# Specify the index_col argument inside pd.read_csv(): set it to 0, so that the first column is used as row labels.
+# Has the printout of cars improved now?
+
+cars = pd.read_csv("../data/cars.csv", index_col=0)
+print(cars)
+# Use single square brackets to print out the country column of cars as a Pandas Series. Use double square brackets
+# to print out the country column of cars as a Pandas DataFrame. Use double square brackets to print out a DataFrame
+# with both the country and drives_right columns of cars, in this order.
+
+# Print out country column as Pandas Series
+print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+
+print(type(cars[['country', 'drives_right']]))
+
+# Print out first 3 observations
+print(cars[0:3])
+
+# Print out fourth, fifth and sixth observation
+print(cars[3:6])
+
+# using loc and iloc
+
+# Use loc or iloc to select the observation corresponding to Japan as a Series. The label of this row is JPN,
+# the index is 2. Make sure to print the resulting Series. Use loc or iloc to select the observations for Australia
+# and Egypt as a DataFrame. You can find out about the labels/indexes of these rows by inspecting cars in the IPython
+# Shell. Make sure to print the resulting DataFrame.
+
+# Print out observation for Japan
+print(cars.loc['JPN'])
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
+print(cars.iloc[[1, 6]])
+
+# Print out the drives_right value of the row corresponding to Morocco (its row label is MOR) Print out a
+# sub-DataFrame, containing the observations for Russia and Morocco and the columns country and drives_right.
+
+# Print out drives_right value of Morocco
+print(cars.loc['MOR', 'drives_right'])
+print(cars.iloc[5, 2])
+
+# Print sub-DataFrame
+print(cars.loc[['RU', 'MOR'], ['country', 'drives_right']])
+print(cars.iloc[[4, 5], [0, 2]])
+
+# Print out the drives_right column as a Series using loc or iloc.
+# Print out the drives_right column as a DataFrame using loc or iloc.
+# Print out both the cars_per_cap and drives_right column as a DataFrame using loc or iloc.
+
+# Print out drives_right column as Series
+print(cars.loc[:, 'drives_right'])
+print(cars.iloc[:, 2])
+
+# Print out drives_right column as DataFrame
+print(cars.loc[:, ['drives_right']])
+print(cars.iloc[:, [2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:, ['cars_per_cap', 'drives_right']])
+print(cars.iloc[:, [1, 2]])
 
