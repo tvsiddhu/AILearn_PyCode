bccc2af16333b997d3988a1bf29fb3bd739a2326
tvsiddhu <tv.siddhu@gmail.com>
2024-10-14 12:02:58 +0800

    Statistical Thinking in Python (Part 2) - Completed

--- src/7.statistical_thinking/stats_thinking_2.py
+++ src/7.statistical_thinking/stats_thinking_2.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+
 # Define a function to compute the ECDF
 def ecdf(data):
     """Compute ECDF for a one-dimensional array of measurements."""
@@ -324,6 +325,7 @@ _ = plt.ylabel('PDF')
 # Show the plot
 plt.show()
 
+
 # 12. A function to do pairs bootstrap
 
 def draw_bs_pairs_linreg(x, y, size=1):
@@ -343,6 +345,7 @@ def draw_bs_pairs_linreg(x, y, size=1):
         bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x, bs_y, 1)
 
     return bs_slope_reps, bs_intercept_reps
+
 
 # 13. Pairs bootstrap of literacy/fertility data
 
@@ -376,6 +379,7 @@ _ = plt.ylabel('fertility')
 plt.margins(0.02)
 plt.show()
 
+
 # Formulating and simulating a hypothesis
 
 # 15. Generating a permutation sample
@@ -394,6 +398,7 @@ def permutation_sample(data1, data2):
     perm_sample_2 = permuted_data[len(data1):]
 
     return perm_sample_1, perm_sample_2
+
 
 # 16. Visualizing permutation sampling
 
@@ -424,6 +429,7 @@ _ = plt.xlabel('monthly rainfall (mm)')
 _ = plt.ylabel('ECDF')
 plt.show()
 
+
 # 17. Generating permutation replicates
 
 def draw_perm_reps(data_1, data_2, func, size=1):
@@ -441,6 +447,7 @@ def draw_perm_reps(data_1, data_2, func, size=1):
 
     return perm_replicates
 
+
 # 18. Look before you leap: EDA before hypothesis testing
 
 # Load the data
@@ -456,6 +463,7 @@ _ = plt.ylabel('impact force (mN)')
 # Show the plot
 plt.show()
 
+
 # 19. Permutation test on frog data
 
 def diff_of_means(data_1, data_2):
@@ -466,8 +474,9 @@ def diff_of_means(data_1, data_2):
 
     return diff
 
-force_a = frog_df.loc[frog_df['ID']=='II', 'impact force (mN)'].values / 1000
-force_b = frog_df.loc[frog_df['ID']=='IV', 'impact force (mN)'].values / 1000
+
+force_a = frog_df.loc[frog_df['ID'] == 'II', 'impact force (mN)'].values / 1000
+force_b = frog_df.loc[frog_df['ID'] == 'IV', 'impact force (mN)'].values / 1000
 
 # Compute difference of mean impact force from experiment: empirical_diff_means
 empirical_diff_means = diff_of_means(force_a, force_b)
@@ -520,4 +529,424 @@ bs_replicates = bs_replicates_a - bs_replicates_b
 
 # Compute and print p-value: p
 p = np.sum(bs_replicates >= empirical_diff_means) / len(bs_replicates)
-print('p-value =', p)
+print('p-value for two-sample bootstrap hypotheses=', p)
+
+# 22. A/B test
+
+# Construct arrays of data: dems, reps
+dems = np.array([True] * 153 + [False] * 91)
+reps = np.array([True] * 136 + [False] * 35)
+
+
+def frac_yea_dems(dems, reps):
+    """Compute fraction of Democrat yea votes."""
+    frac = np.sum(dems) / len(dems)
+    return frac
+
+
+# Acquire permutation samples: perm_replicates
+perm_replicates = draw_perm_reps(dems, reps, frac_yea_dems, 10000)
+
+# Compute and print p-value: p
+p = np.sum(perm_replicates <= 153 / 244) / len(perm_replicates)
+print('p-value for dems who were voting yea=', p)
+
+
+# 23. A time-on-website analog
+
+def draw_perm_reps(data_1, data_2, func, size=1):
+    """Generate multiple permutation replicates."""
+
+    # Initialize array of replicates
+    perm_replicates = np.empty(size)
+
+    for i in range(size):
+        # Generate permutation sample
+        perm_sample_1, perm_sample_2 = permutation_sample(data_1, data_2)
+
+        # Compute the test statistic
+        perm_replicates[i] = func(perm_sample_1, perm_sample_2)
+
+    return perm_replicates
+
+
+# Compute the observed difference in mean inter-no-hitter times: nht_diff_obs
+nht_dead = np.array([-1, 894, 10, 130, 1, 934, 29, 6, 485, 254, 372,
+                     81, 191, 355, 180, 286, 47, 269, 361, 173, 246, 492,
+                     462, 1319, 58, 297, 31, 2970, 640, 237, 434, 570, 77,
+                     271, 563, 3365, 89, 0, 379, 221, 479, 367, 628, 843,
+                     1613, 1101, 215, 684, 814, 278, 324, 161, 219, 545, 715,
+                     966, 624, 29, 450, 107, 20, 91, 1325, 124, 1468, 104,
+                     1309, 429, 62, 1878, 1104, 123, 251, 93, 188, 983, 166,
+                     96, 702, 23, 524, 26, 299, 59, 39, 12, 2, 308,
+                     1114, 813, 887])
+
+nht_live = np.array([645, 2088, 42, 2090, 11, 886, 1665, 1084, 2900, 2432, 750,
+                     4021, 1070, 1765, 1322, 26, 548, 1525, 77, 2181, 2752, 127,
+                     2147, 211, 41, 1575, 151, 479, 697, 557, 2267, 542, 392,
+                     73, 603, 233, 255, 528, 397, 1529, 1023, 1194, 462, 583,
+                     37, 943, 996, 480, 1497, 717, 224, 219, 1531, 498, 44,
+                     288, 267, 600, 52, 269, 1086, 386, 176, 2199, 216, 54,
+                     675, 1243, 463, 650, 171, 327, 110, 774, 509, 8, 197,
+                     136, 12, 1124, 64, 380, 811, 232, 192, 731, 715, 226,
+                     605, 539, 1491, 323, 240, 179, 702, 156, 82, 1397, 354,
+                     778, 603, 1001, 385, 986, 203, 149, 576, 445, 180, 1403,
+                     252, 675, 1351, 2983, 1568, 45, 899, 3260, 1025, 31, 100,
+                     2055, 4043, 79, 238, 3931, 2351, 595, 110, 215, 0, 563,
+                     206, 660, 242, 577, 179, 157, 192, 192, 1848, 792, 1693,
+                     55, 388, 225, 1134, 1172, 1555, 31, 1582, 1044, 378, 1687,
+                     2915, 280, 765, 2819, 511, 1521, 745, 2491, 580, 2072, 6450,
+                     578, 745, 1075, 1103, 1549, 1520, 138, 1202, 296, 277, 351,
+                     391, 950, 459, 62, 1056, 1128, 139, 420, 87, 71, 814,
+                     603, 1349, 162, 1027, 783, 326, 101, 876, 381, 905, 156,
+                     419, 239, 119, 129, 467])
+
+# Compute the observed difference in mean inter-no-hitter times: nht_diff_obs
+nht_diff_obs = diff_of_means(nht_dead, nht_live)
+
+# Acquire 10,000 permutation replicates of difference in mean no-hitter time: perm_replicates
+perm_replicates = draw_perm_reps(nht_dead, nht_live, diff_of_means, 10000)
+
+# Compute and print the p-value: p
+p = np.sum(perm_replicates <= nht_diff_obs) / len(perm_replicates)
+print('p-val of no-hitter times =', p)
+
+# 24. Hypothesis test on Pearson correlation
+
+# Compute observed correlation: r_obs
+r_obs = pearson_r(illiteracy, fertility)
+
+# Initialize permutation replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute illiteracy measurments: illiteracy_permuted
+    illiteracy_permuted = np.random.permutation(illiteracy)
+
+    # Compute Pearson correlation
+    perm_replicates[i] = pearson_r(illiteracy_permuted, fertility)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= r_obs) / len(perm_replicates)
+print('p-val of Pearson correlation =', p)
+
+# 25. Do neonicotinoid insecticides have unintended consequences?
+
+bee_df = pd.read_csv('../../data/stats_thinking_sources/bee_sperm.csv')
+
+control = bee_df[bee_df['Treatment'] == 'Control']['Alive Sperm Millions']
+treated = bee_df[bee_df['Treatment'] == 'Pesticide']['Alive Sperm Millions']
+
+# Compute x,y values for ECDFs
+x_control, y_control = ecdf(control)
+x_treated, y_treated = ecdf(treated)
+
+# Plot the ECDFs
+plt.plot(x_control, y_control, marker='.', linestyle='none')
+plt.plot(x_treated, y_treated, marker='.', linestyle='none')
+
+# Set the margins
+plt.margins(0.02)
+
+# Add a legend
+plt.legend(('control', 'treated'), loc='lower right')
+
+# Label axes and show plot
+plt.xlabel('millions of alive sperm per mL')
+plt.ylabel('ECDF')
+plt.show()
+
+# 26. Bootstrap hypothesis test on bee sperm counts
+
+# Compute the difference in mean sperm count: diff_means
+diff_means = diff_of_means(control, treated)
+
+# Compute mean of pooled data: mean_count
+mean_count = np.mean(np.concatenate((control, treated)))
+
+# Generate shifted data sets
+control_shifted = control - np.mean(control) + mean_count
+treated_shifted = treated - np.mean(treated) + mean_count
+
+# Generate bootstrap replicates
+bs_reps_control = draw_bs_reps(control_shifted, np.mean, 10000)
+bs_reps_treated = draw_bs_reps(treated_shifted, np.mean, 10000)
+
+# Get replicates of difference of means: bs_replicates
+bs_replicates = bs_reps_control - bs_reps_treated
+
+# Compute and print p-value: p
+p = np.sum(bs_replicates >= diff_means) / len(bs_replicates)
+print('p-val of bee sperm counts =', p)
+
+# 27. EDA of beak depths of Darwin's finches
+
+# Load the data
+finch_beaks_1975 = pd.read_csv('../../data/stats_thinking_sources/finch_beaks_1975.csv')
+finch_beaks_2012 = pd.read_csv('../../data/stats_thinking_sources/finch_beaks_2012.csv')
+
+# Create bee swarm plot
+_ = sns.swarmplot(x='year', y='Beak depth, mm', data=pd.concat([finch_beaks_1975.assign(year=1975),
+                                                                finch_beaks_2012.assign(year=2012)]))
+
+# Label the axes
+_ = plt.xlabel('year')
+_ = plt.ylabel('beak depth (mm)')
+plt.show()
+
+# 28. ECDFs of beak depths
+
+# Compute ECDFs
+x_1975, y_1975 = ecdf(finch_beaks_1975['Beak depth, mm'])
+x_2012, y_2012 = ecdf(finch_beaks_2012['bdepth'])
+
+# Plot the ECDFs
+plt.plot(x_1975, y_1975, marker='.', linestyle='none')
+plt.plot(x_2012, y_2012, marker='.', linestyle='none')
+
+# Set margins
+plt.margins(0.02)
+
+# Add axis labels and legend
+plt.xlabel('beak depth (mm)')
+plt.ylabel('ECDF')
+plt.legend(('1975', '2012'), loc='lower right')
+
+# Show the plot
+plt.show()
+
+# 29. Parameter estimates of beak depths
+
+# Compute the difference of the sample means: mean_diff
+mean_diff = np.mean(finch_beaks_2012['bdepth']) - np.mean(finch_beaks_1975['Beak depth, mm'])
+
+# Get bootstrap replicates of means
+bs_replicates_1975 = draw_bs_reps(finch_beaks_1975['Beak depth, mm'], np.mean, 10000)
+bs_replicates_2012 = draw_bs_reps(finch_beaks_2012['bdepth'], np.mean, 10000)
+
+# Compute samples of difference of means: bs_diff_replicates
+bs_diff_replicates = bs_replicates_2012 - bs_replicates_1975
+
+# Compute 95% confidence interval: conf_int
+conf_int = np.percentile(bs_diff_replicates, [2.5, 97.5])
+
+# Print the results
+print('difference of means =', mean_diff, 'mm')
+print('95% confidence interval =', conf_int, 'mm')
+
+# 30. Hypothesis test: Are beaks deeper in 2012?
+
+# Compute mean of combined data set: combined_mean
+combined_mean = np.mean(np.concatenate((finch_beaks_1975['Beak depth, mm'], finch_beaks_2012['bdepth'])))
+
+# Shift the samples
+bd_1975_shifted = finch_beaks_1975['Beak depth, mm'] - np.mean(finch_beaks_1975['Beak depth, mm']) + combined_mean
+bd_2012_shifted = finch_beaks_2012['bdepth'] - np.mean(finch_beaks_2012['bdepth']) + combined_mean
+
+# Get bootstrap replicates of shifted data sets
+bs_replicates_1975 = draw_bs_reps(bd_1975_shifted, np.mean, 10000)
+bs_replicates_2012 = draw_bs_reps(bd_2012_shifted, np.mean, 10000)
+
+# Compute replicates of difference of means: bs_diff_replicates
+bs_diff_replicates = bs_replicates_2012 - bs_replicates_1975
+
+# Compute the p-value
+p = np.sum(bs_diff_replicates >= mean_diff) / len(bs_diff_replicates)
+
+# Print p-value
+print('p =', p)
+
+# 31. EDA of beak length and depth
+
+bl_1975 = finch_beaks_1975['Beak length, mm']
+bd_1975 = finch_beaks_1975['Beak depth, mm']
+bl_2012 = finch_beaks_2012['blength']
+bd_2012 = finch_beaks_2012['bdepth']
+
+# Make scatter plot of 1975 data
+_ = plt.plot(bl_1975, bd_1975, marker='.', linestyle='none', color='blue', alpha=0.5)
+
+# Make scatter plot of 2012 data
+_ = plt.plot(bl_2012, bd_2012, marker='.', linestyle='none', color='red', alpha=0.5)
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Show the plot
+plt.show()
+
+# 32. Linear regressions
+
+# Compute the linear regressions
+slope_1975, intercept_1975 = np.polyfit(bl_1975, bd_1975, 1)
+slope_2012, intercept_2012 = np.polyfit(bl_2012, bd_2012, 1)
+
+# Perform pairs bootstrap for the linear regressions
+bs_slope_reps_1975, bs_intercept_reps_1975 = draw_bs_pairs_linreg(bl_1975, bd_1975, 1000)
+bs_slope_reps_2012, bs_intercept_reps_2012 = draw_bs_pairs_linreg(bl_2012, bd_2012, 1000)
+
+# Compute confidence intervals of slopes
+slope_conf_int_1975 = np.percentile(bs_slope_reps_1975, [2.5, 97.5])
+slope_conf_int_2012 = np.percentile(bs_slope_reps_2012, [2.5, 97.5])
+intercept_conf_int_1975 = np.percentile(bs_intercept_reps_1975, [2.5, 97.5])
+intercept_conf_int_2012 = np.percentile(bs_intercept_reps_2012, [2.5, 97.5])
+
+# Print the results
+print('1975: slope =', slope_1975, 'conf int =', slope_conf_int_1975)
+print('1975: intercept =', intercept_1975, 'conf int =', intercept_conf_int_1975)
+print('2012: slope =', slope_2012, 'conf int =', slope_conf_int_2012)
+print('2012: intercept =', intercept_2012, 'conf int =', intercept_conf_int_2012)
+
+# 33. Displaying the linear regression results
+
+# Make scatter plot of 1975 data
+_ = plt.plot(bl_1975, bd_1975, marker='.', linestyle='none', color='blue', alpha=0.5)
+
+# Make scatter plot of 2012 data
+_ = plt.plot(bl_2012, bd_2012, marker='.', linestyle='none', color='red', alpha=0.5)
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Generate x-values for bootstrap lines: x
+x = np.array([10, 17])
+
+# Plot the bootstrap lines
+for i in range(100):
+    plt.plot(x, bs_slope_reps_1975[i] * x + bs_intercept_reps_1975[i], linewidth=0.5, alpha=0.2, color='blue')
+    plt.plot(x, bs_slope_reps_2012[i] * x + bs_intercept_reps_2012[i], linewidth=0.5, alpha=0.2, color='red')
+
+# Draw the plot again
+plt.show()
+
+# 34. Beak length to depth ratio
+
+# Compute length-to-depth ratios
+ratio_1975 = bl_1975 / bd_1975
+ratio_2012 = bl_2012 / bd_2012
+
+# Compute means
+mean_ratio_1975 = np.mean(ratio_1975)
+mean_ratio_2012 = np.mean(ratio_2012)
+
+# Generate bootstrap replicates of the means
+bs_replicates_1975 = draw_bs_reps(ratio_1975, np.mean, 10000)
+bs_replicates_2012 = draw_bs_reps(ratio_2012, np.mean, 10000)
+
+# Compute the 99% confidence intervals
+conf_int_1975 = np.percentile(bs_replicates_1975, [0.5, 99.5])
+conf_int_2012 = np.percentile(bs_replicates_2012, [0.5, 99.5])
+
+# Print the results
+print('1975: mean ratio =', mean_ratio_1975, 'conf int =', conf_int_1975)
+print('2012: mean ratio =', mean_ratio_2012, 'conf int =', conf_int_2012)
+
+# 35. EDA of heritability
+
+# Load the data
+scanden_df = pd.read_csv('../../data/stats_thinking_sources/scandens_beak_depth_heredity.csv')
+fortis_df = pd.read_csv('../../data/stats_thinking_sources/fortis_beak_depth_heredity.csv')
+
+bd_offspring_scandens = scanden_df['mid_parent']
+bd_parent_scandens = scanden_df['mid_offspring']
+
+bd_offspring_fortis = fortis_df['Mid-offspr']
+bd_parent_fortis = np.mean([fortis_df['Male BD'], fortis_df['Female BD']], axis=0)
+
+# Make scatter plots
+_ = plt.plot(bd_parent_fortis, bd_offspring_fortis,
+             marker='.', linestyle='none', color='blue', alpha=0.5)
+_ = plt.plot(bd_parent_scandens, bd_offspring_scandens,
+             marker='.', linestyle='none', color='red', alpha=0.5)
+
+# Label axes
+_ = plt.xlabel('parental beak depth (mm)')
+_ = plt.ylabel('offspring beak depth (mm)')
+
+# Add legend
+_ = plt.legend(('G. fortis', 'G. scandens'), loc='lower right')
+
+# Show plot
+plt.show()
+
+
+# 36. Correlation of offspring and parental data
+
+def draw_bs_pairs(x, y, func, size=1):
+    """Perform pairs bootstrap for a single statistic."""
+
+    # Set up array of indices to sample from
+    inds = np.arange(len(x))
+
+    # Initialize replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_replicates[i] = func(bs_x, bs_y)
+
+    return bs_replicates
+
+# 37. Pearson correlation of offspring and parental data
+
+# Compute the Pearson correlation coefficients
+r_scandens = pearson_r(bd_parent_scandens, bd_offspring_scandens)
+r_fortis = pearson_r(bd_parent_fortis, bd_offspring_fortis)
+
+# Acquire 1000 bootstrap replicates of Pearson r
+bs_replicates_scandens = draw_bs_pairs(bd_parent_scandens, bd_offspring_scandens, pearson_r, 1000)
+bs_replicates_fortis = draw_bs_pairs(bd_parent_fortis, bd_offspring_fortis, pearson_r, 1000)
+
+# Compute 95% confidence intervals
+conf_int_scandens = np.percentile(bs_replicates_scandens, [2.5, 97.5])
+conf_int_fortis = np.percentile(bs_replicates_fortis, [2.5, 97.5])
+
+# Print results
+print('G. scandens:', r_scandens, conf_int_scandens)
+print('G. fortis:', r_fortis, conf_int_fortis)
+
+# 38. Measuring heritability
+
+# Define heritability
+def heritability(parents, offspring):
+    """Compute the heritability from parent and offspring samples."""
+    covariance_matrix = np.cov(parents, offspring)
+    return covariance_matrix[0, 1] / covariance_matrix[0, 0]
+
+# Compute the heritability
+heritability_scandens = heritability(bd_parent_scandens, bd_offspring_scandens)
+heritability_fortis = heritability(bd_parent_fortis, bd_offspring_fortis)
+
+# Acquire 1000 bootstrap replicates of heritability
+replicates_scandens = draw_bs_pairs(bd_parent_scandens, bd_offspring_scandens, heritability, 1000)
+replicates_fortis = draw_bs_pairs(bd_parent_fortis, bd_offspring_fortis, heritability, 1000)
+
+# Compute 95% confidence intervals
+conf_int_scandens = np.percentile(replicates_scandens, [2.5, 97.5])
+conf_int_fortis = np.percentile(replicates_fortis, [2.5, 97.5])
+
+# Print results
+print('Heritability of G. scandens:', heritability_scandens, conf_int_scandens)
+print('Heritability of G. fortis:', heritability_fortis, conf_int_fortis)
+
+# 39. Is beak depth heritable at all in G. scandens?
+
+# Initialize array of replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute parent beak depths
+    bd_parent_permuted = np.random.permutation(bd_parent_scandens)
+    perm_replicates[i] = heritability(bd_parent_permuted, bd_offspring_scandens)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= heritability_scandens) / len(perm_replicates)
+print('p-val of heritability of G. scandens =', p)
